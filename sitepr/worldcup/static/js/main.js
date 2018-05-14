2735d818e20c031c959c190b227529e3b1487b19
ndarriulat <nicolasd92@gmail.com>
2018-05-14 01:21:23 +0100

    Login popup returns to same page when it is successful

--- sitepr/worldcup/static/js/main.js
+++ sitepr/worldcup/static/js/main.js
@@ -1,19 +1,48 @@
 var a= 0;
 $(document).ready(function(){
-//     $(".navbar__mobile").click(function(){
-//        if(a==0){
-//            $(".menu").slideDown("slow");
+    
+     $('.message a').click(function(){
+        $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+     });
+     
+//    $('.nav-login').click(function(){
+//        $('.tte').css("display", "block");
+//     });
+    
+    $('.nav-login').on('click touch',function(){
+        $('.tte').css("display", "block");
+     });
+    
+    $('.ttte').click(function(){
+        $('.tte').css("display", "none");
+     });
+    
+
+    
+    
+    
+    
+     $(".navbar__mobile").click(function(){
+        if(a==0){
+            $(".menu").slideDown("slow");
 //            $( ".group__table" ).fadeTo( "slow" , 1, function() {
 //    // Animation complete.
 //  });
 //             $( ".group__container" ).fadeTo( "slow" , 1, function() {
 //    // Animation complete.
 //  });
-//            a++;
-//        }else{
-//            $(".menu").slideUp("slow");
-//            a--;
-//        }
+            a++;
+        }else{
+            $(".menu").slideUp("slow");
+            a--;
+        }
     
-//    });
+    });
+    
+    
 });
+
+function myFunction(){
+    $('.tte').css("display", "block");
+}
+
